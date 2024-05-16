cd1d3d958f23003a8fd0b6ec530d38c93a3259dd
amalozemov174 <95954639+amalozemov174@users.noreply.github.com>
2024-05-16 15:13:22 +0500

    Update task2.sql

--- task2.sql
+++ task2.sql
@@ -1,33 +1,47 @@
--- Task1
--- Задание 1. Выведите уникальные названия городов из таблицы городов.
-select distinct city from city;
--- Задание 2. Доработайте запрос из предыдущего задания, чтобы запрос выводил только те города, названия которых начинаются на “L” и заканчиваются на “a”, и названия не содержат пробелов.
-select distinct city from city where city like 'L%a';
--- Задание 3. Получите из таблицы платежей за прокат фильмов информацию по платежам, которые выполнялись в промежуток с 17 июня 2005 года по 19 июня 2005 года включительно и стоимость которых превышает 1.00. Платежи нужно отсортировать по дате платежа.
-select * from payment where payment_date between to_date('2005-06-17', 'YYYY-MM-DD') and to_date('2005-06-20', 'YYYY-MM-DD') and amount > 1.00 order by payment_date;
--- Задание 4. Выведите информацию о 10-ти последних платежах за прокат фильмов.
-select * from payment order by payment_id desc limit 5;
--- Задание 5. Выведите следующую информацию по покупателям:
--- •	Фамилия и имя (в одной колонке через пробел)
--- •	Электронная почта
--- •	Длину значения поля email
--- •	Дату последнего обновления записи о покупателе (без времени)
---  Каждой колонке задайте наименование на русском языке.
-select first_name || ' ' || last_name as "ФИО", email as "Почта", length(email) as "Длина почты", last_update as "Последнее обновление" from customer;
--- Задание 6. Выведите одним запросом только активных покупателей, имена которых KELLY или WILLIE. Все буквы в фамилии и имени из верхнего регистра должны быть переведены в нижний регистр.
-select customer_id, store_id, lower(first_name), lower(last_name), email, address_id, activebool, create_date, last_update, active from customer where active = 1 and first_name in ('KELLY', 'WILLIE');
--- Дополнительная часть:
--- Задание 7. Выведите одним запросом информацию о фильмах, у которых рейтинг “R” и стоимость аренды указана от 0.00 до 3.00 включительно, а также фильмы c рейтингом “PG-13” и стоимостью аренды больше или равной 4.00.
-select * from film where rating = 'R' and rental_rate between 0.00 and 3.00 or rating = 'PG-13' and rental_rate >= 4.00;
-
--- Задание 8. Получите информацию о трёх фильмах с самым длинным описанием фильма.
-select * from film order by length(description) desc limit 3;
--- Задание 9. Выведите Email каждого покупателя, разделив значение Email на 2 отдельных колонки:
--- 	в первой колонке должно быть значение, указанное до @,
--- 	во второй колонке должно быть значение, указанное после @.
-select  substring(email, 1, position('@' in email) - 1) as first, substring(email, position('@' in email) + 1, length(email)) as second from customer;
---Задание 10. Доработайте запрос из предыдущего задания, скорректируйте значения в новых колонках: первая буква должна быть заглавной, остальные строчными.
-select  
-upper(substring(lower(substring(email, 1, position('@' in email) - 1)), 1, 1)) || substring(lower(substring(email, 1, position('@' in email) - 1)), 2, length(substring(email, 1, position('@' in email) - 1))) as first, 
-upper(substring(lower(substring(email, position('@' in email) + 1, length(email))), 1, 1)) || substring(lower(substring(email, position('@' in email) + 1, length(email))), 2, length(substring(email, position('@' in email) + 1, length(email)))) as second
-from customer;
+-- Task2
+-- Задание 1. Выведите для каждого покупателя его адрес, город и страну проживания.
+select address.address, city.city, country.country from customer, address, city, country
+where customer.address_id = address.address_id and address.city_id = city.city_id and city.country_id = country.country_id;
+-- Задание 2. С помощью SQL-запроса посчитайте для каждого магазина количество его покупателей.
+select store_id, count(customer) from customer group by store_id;
+-- Доработайте запрос и выведите только те магазины, у которых количество покупателей больше 300. Для решения используйте фильтрацию по сгруппированным строкам с функцией агрегации. 
+select store_id, count(customer) from customer group by store_id having count(customer) > 300;
+-- Доработайте запрос, добавив в него информацию о городе магазина, фамилии и имени продавца, который работает в нём.
+select store.store_id, count(customer.customer_id), city.city, staff.last_name, staff.first_name from customer, address, store, city, staff 
+where customer.store_id = store.store_id and store.address_id = address.address_id and address.city_id = city.city_id and store.manager_staff_id = staff.staff_id
+group by store.store_id, city.city, staff.last_name, staff.first_name having count(customer.customer_id) > 300;
+-- Задание 3. Выведите топ-5 покупателей, которые взяли в аренду за всё время наибольшее количество фильмов.
+select customer_id, count(customer_id) from payment
+group by customer_id
+order by count(customer_id) desc
+limit 5;
+-- Задание 4. Посчитайте для каждого покупателя 4 аналитических показателя:
+-- количество взятых в аренду фильмов;
+-- общую стоимость платежей за аренду всех фильмов (значение округлите до целого числа);
+-- минимальное значение платежа за аренду фильма;
+-- максимальное значение платежа за аренду фильма.
+select customer_id, count(rental_id), sum(amount), min(amount), max(amount) from payment
+group by customer_id;
+-- Задание 5. Используя данные из таблицы городов, составьте одним запросом всевозможные пары городов так, чтобы в результате не было пар с одинаковыми названиями городов. Для решения необходимо использовать декартово произведение.
+select c.city, c1.city
+from city c
+cross join city c1
+where c.city != c1.city;
+-- Задание 6. Используя данные из таблицы rental о дате выдачи фильма в аренду (поле rental_date) и дате возврата (поле return_date), вычислите для каждого покупателя среднее количество дней, за которые он возвращает фильмы.
+select customer_id, round(avg(DATE_PART('day', return_date - rental_date))) as avg_return from rental 
+group by customer_id;
+-- Задание 7. Посчитайте для каждого фильма, сколько раз его брали в аренду, а также общую стоимость аренды фильма за всё время.
+select inventory.film_id, count(rental.rental_id), sum(payment.amount) from inventory, rental, payment
+where inventory.inventory_id = rental.inventory_id and rental.rental_id = payment.rental_id
+group by inventory.film_id;
+-- Задание 8. Доработайте запрос из предыдущего задания и выведите с помощью него фильмы, которые ни разу не брали в аренду.
+select inventory.film_id, inventory.inventory_id from inventory
+left join rental on inventory.inventory_id = rental.inventory_id
+where rental.inventory_id is null;
+-- Задание 9. Посчитайте количество продаж, выполненных каждым продавцом. Добавьте вычисляемую колонку «Премия». Если количество продаж превышает 7 300, то значение в колонке будет «Да», иначе должно быть значение «Нет».
+select staff_id, count(payment_id),
+case when count(payment_id) > 7300 THEN 'Да'
+else 'Нет'
+end "'Премия'"
+from payment 
+group by staff_id;
